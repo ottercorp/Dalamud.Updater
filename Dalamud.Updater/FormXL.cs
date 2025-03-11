cdd67e34eedd7025a26b53e2b5cdabdc17981bb8
Loskh <1020612624@qq.com>
2025-03-12 01:25:05 +0800

    🎵add BGM 🎵

--- Dalamud.Updater/FormXL.cs
+++ Dalamud.Updater/FormXL.cs
@@ -5,22 +5,73 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Dalamud.Updater
 {
+    public class MidiPlayer
+    {
+        // Import necessary functions from winmm.dll
+        [DllImport("winmm.dll", SetLastError = true)]
+        private static extern long mciSendString(string command, StringBuilder returnValue, int returnLength, IntPtr winHandle);
+
+        public static void PlayMidi(string fileName)
+        {
+            string command = $"open \"{fileName}\" type sequencer alias midi";
+            mciSendString(command, null, 0, IntPtr.Zero);
+            mciSendString("play midi", null, 0, IntPtr.Zero);
+        }
+
+        public static void StopMidi()
+        {
+            mciSendString("stop midi", null, 0, IntPtr.Zero);
+            mciSendString("close midi", null, 0, IntPtr.Zero);
+        }
+
+        public static string ExtractEmbeddedResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            string tempFile = Path.Combine(Path.GetTempPath(), resourceName);
+
+            using (Stream resourceStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resourceStream == null) throw new ArgumentException($"Resource {resourceName} not found.");
+                using (FileStream fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write))
+                {
+                    resourceStream.CopyTo(fileStream);
+                }
+            }
+
+            return tempFile;
+        }
+    }
+
     public partial class FormXL : Form
     {
         public FormXL()
         {
             InitializeComponent();
+            try
+            {
+                string midiFile = MidiPlayer.ExtractEmbeddedResource(@"Dalamud.Updater.Resources.48186_One-Last-Kiss2.mid");
+                MidiPlayer.PlayMidi(midiFile);
+            }
+            catch { }
         }
 
         private void FormXL_FormClosed(object sender, FormClosedEventArgs e)
         {
+            try {
+                MidiPlayer.StopMidi(); 
+            }
+            catch {
+            }
             //Application.Exit();
         }
 
